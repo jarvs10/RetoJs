d6d6d11721e0e3839f1e876e4e237c69ef59122b
Jaiber <jarvs1009@hotmail.com>
2022-11-18 15:09:34 -0500

    array

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,6 +4,7 @@ const Movies = ({Title, Year, Poster}) => {
 
     return (
         <div className='flex justify-center items-center'>
+            <h1 className='text-5xl font-black'>My Batman Movies</h1>
             <ul className='mt-4 p-2 mb-10 text-center'>
                 <li className='text-2xl font-black'>{Title}</li>
                 <li className='text-1xl font-bold mb-2'>{Year}</li>
